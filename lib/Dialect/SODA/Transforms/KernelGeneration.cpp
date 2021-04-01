33b7bcd90d37d4f51e9cb68a1d2de339849fac23
Nicolas Agostini <nicolas.agostini@pnnl.gov>
2021-04-01 14:50:50 -0700

    Implement -soda-generate-bambu-accelcode functionality
    Isolating the soda kernels so they can be used by the next compilations steps for the bambu target.

--- lib/Dialect/SODA/Transforms/KernelGeneration.cpp
+++ lib/Dialect/SODA/Transforms/KernelGeneration.cpp
@@ -6,9 +6,9 @@
 //
 //===----------------------------------------------------------------------===//
 //
-// This file implements the SODA dialect kernel genration pass.
+// This file implements the SODA dialect kernel genration for Bambu pass.
 //
-// It pull the kernel code into a completely isolated, clean of soda operations
+// It pulls the kernel code into a completely isolated, clean of soda operations
 // mlir file that can be further optimized by regular mlir-opt and subsequently
 // lowered into llvm ir.
 //
@@ -34,12 +34,84 @@ namespace {
 class SodaKernelGenerationPass
     : public SodaKernelGenerationBase<SodaKernelGenerationPass> {
 public:
-  void runOnOperation() override {
-    // TODO(nico): Finish implementation
-  }
+  void runOnOperation() override;
 };
 
 } // namespace
+
+void SodaKernelGenerationPass::runOnOperation() {
+
+  // Steps:
+  // 1 Transfer SODAModuleOp region to its parent mlir::ModuleOp
+  // 2 Delete any code unrelated to the kernel
+  // 3 Walk through the module and change soda.module terminator
+  // 4 Walk through the module and change soda.func to regular func
+  // 5 Walk through the module and change soda.return
+  // 6 Set attribute marking that we modified this mlir:ModuleOp for Bambu
+  // target
+
+  Operation *op = getOperation();
+
+  if (!isa<ModuleOp>(op)) {
+    return signalPassFailure();
+  }
+
+  ModuleOp mop = dyn_cast<ModuleOp>(op);
+  if (!mop) {
+    return signalPassFailure();
+  }
+
+  // Will be set to true if the current module has a SODAModuleOp
+  bool modified = false;
+  mop.walk([this, &modified, &mop](soda::SODAModuleOp sodaOp) {
+    if (modified) {
+      sodaOp.emitError("should only contain one 'soda::SODAModuleOp' op");
+      return signalPassFailure();
+    }
+
+    BlockAndValueMapping map;
+    sodaOp.body().cloneInto(&(mop.body()), map);
+    sodaOp.erase();
+
+    modified = true;
+  });
+
+  // This module does not have a SODAModuleOp
+  if (!modified) {
+    return signalPassFailure();
+  }
+
+  // We inserted a new block into a ModuleOp, ModuleOp should have only one
+  // block, thus delete the old (first) block. This also takes care of deleting
+  // the nested SODAModuleOp or other unrelated code.
+  Block *oldBlock = &mop->getRegions().front().getBlocks().front();
+  oldBlock->erase();
+
+  mop.walk([](soda::ModuleEndOp endOp) {
+    OpBuilder replacer(endOp);
+    replacer.create<mlir::ModuleTerminatorOp>(endOp.getLoc());
+    endOp.erase();
+  });
+
+  mop.walk([](soda::SODAFuncOp funcOp) {
+    OpBuilder replacer(funcOp);
+
+    FuncOp dstFunc = replacer.create<mlir::FuncOp>(
+        funcOp.getLoc(), funcOp.getName(), funcOp.getType());
+
+    dstFunc.body().takeBody(funcOp.body());
+    funcOp.erase();
+  });
+
+  mop.walk([](soda::ReturnOp returnOp) {
+    OpBuilder replacer(returnOp);
+    replacer.create<mlir::ReturnOp>(returnOp.getLoc());
+    returnOp.erase();
+  });
+
+  if (modified)
+    mop->setAttr("soda.bambu.container_module", UnitAttr::get(&getContext()));
+}
 
 std::unique_ptr<OperationPass<ModuleOp>>
 mlir::soda::createSodaKernelGenerationPass() {
--- test/Dialect/SODA/kernel-generation.mlir
+++ test/Dialect/SODA/kernel-generation.mlir
@@ -1,8 +1,6 @@
-// RUN: soda-opt -allow-unregistered-dialect -soda-outline-bambu-code -soda-generate-bambu-accelcode %s | FileCheck %s
+// RUN: soda-opt -split-input-file -allow-unregistered-dialect -soda-outline-bambu-code -soda-generate-bambu-accelcode %s | FileCheck %s
 
-// CHECK: module attributes {soda.container_module}
-
-// CHECK-LABEL: func @driver()
+// CHECK: module attributes {soda.bambu.container_module
 func @driver() {
   // CHECK-NOT: "loadA"
   %A = "loadA"() : () -> (memref<512x512xf32>)
@@ -11,13 +9,36 @@ func @driver() {
   // CHECK-NOT: "allocateC"
   %C = "allocateC"() : () -> (memref<512x512xf32>)
 
-  // CHECK-NOT: soda.launch blocks
-  // CHECK-NOT: soda.launch_func blocks
+  // CHECK-NOT: "soda.func"
   soda.launch  {
     linalg.matmul ins(%A, %B : memref<512x512xf32>, memref<512x512xf32>)
                     outs(%C : memref<512x512xf32>)
     soda.terminator
   }
+  // CHECK-NOT: "soda.return"
 
   return
 }
+
+// -----
+
+// CHECK: module attributes {soda.bambu.container_module
+func @complex() {
+  %A = "loadA"() : () -> (memref<512x512xf32>)
+  %B = call @init_b(): () -> (memref<512x512xf32>)
+
+  soda.launch  {
+    // CHECK call @init_c
+    %C = call @init_c(): () -> (memref<512x512xf32>)
+    linalg.matmul ins(%A, %B : memref<512x512xf32>, memref<512x512xf32>)
+                    outs(%C : memref<512x512xf32>)
+    soda.terminator
+  }
+  // CHECK-NOT: "soda.return"
+  return
+}
+
+// CHECK-NOT: func private @init_b
+func private @init_b() -> (memref<512x512xf32>)
+// CHECK: func private @init_c
+func private @init_c() -> (memref<512x512xf32>)
