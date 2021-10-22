ea94d4abdfb2457bac23a2fa56454cbef4ee4526
Nicolas Agostini <n.b.agostini@gmail.com>
2021-10-21 17:58:46 -0700

    Add flexible scripts to gemm example

---
+++ experiments/polybench/code/gemm/scripts/run-outline-affine_for-opt_full-bambu-soft_float-no_ssdcs.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# USAGE
+#  ./<file-name>.sh <kernel size>
+
+# Definition
+#  Calls the scripts to perform soda-opt compilation and bambu synthesis
+
+# Kernel configs
+NAME=gemm
+KSIZE=$1
+
+# Getting the kernel directory
+KERNELDIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"/..
+
+# Bambu configs
+source ${KERNELDIR}/../../scripts/bambu-config-values.sh
+
+# Load debug flags for bambu
+source ${KERNELDIR}/../../scripts/bambu-debug-flags.sh
+
+# Execute
+source ${KERNELDIR}/../../scripts/outline-affine_for-opt_full-bambu-soft_float-no_ssdcs.sh
