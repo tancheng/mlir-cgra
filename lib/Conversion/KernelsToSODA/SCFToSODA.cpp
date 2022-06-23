fd464929c3c7dd057b9a49434a668c2cf7ec9540
Nicolas Agostini <n.b.agostini@gmail.com>
2022-06-22 18:19:35 -0700

    Update include on SCF to SODA pass

--- lib/Conversion/KernelsToSODA/SCFToSODA.cpp
+++ lib/Conversion/KernelsToSODA/SCFToSODA.cpp
@@ -10,7 +10,7 @@
 #include "soda/Dialect/SODA/SODADialect.h"
 
 #include "mlir/Dialect/Affine/IR/AffineOps.h"
-#include "mlir/Dialect/SCF/SCF.h"
+#include "mlir/Dialect/SCF/IR/SCF.h"
 #include "mlir/IR/Builders.h"
 #include "mlir/IR/Location.h"
 
