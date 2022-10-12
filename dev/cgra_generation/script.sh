ed2b041d1b3c7f8f3210cf23014cad69b7bff160
tancheng <tancheng1990@gmail.com>
2022-10-12 14:54:24 -0700

    [update] script and output

--- dev/cgra_generation/script.sh
+++ dev/cgra_generation/script.sh
@@ -1,3 +1,3 @@
 
 # [linalg -> cgra]
-soda-opt --convert-linalg-matmul-to-soda --convert-linalg-generic-to-soda gemmreluadd.linalg.tiled.mlir > gemmreluadd.cgra.mlir
+soda-opt --convert-linalg-matmul-to-cgra --convert-pattern-to-cgra=target-patterns="arith.addf-arith.maxf-arith.addf-linalg.yield" gemmreluadd.linalg.tiled.mlir > gemmreluadd.cgra.mlir
