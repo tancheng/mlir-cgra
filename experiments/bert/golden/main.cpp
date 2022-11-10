c8b9c5e48af3a5bbf6fb368111abaa3c218c2c68
tancheng <tancheng1990@gmail.com>
2022-11-10 11:01:14 -0800

    [evaluate] BERT step-by-step

--- experiments/bert/golden/main.cpp
+++ experiments/bert/golden/main.cpp
@@ -12,59 +12,39 @@ template <typename T, size_t N> struct MemRefDescriptor {
 };
 
 extern "C" {
-  // void *main_graph(MemRefDescriptor<float, 2> input0, MemRefDescriptor<float, 2> input1, MemRefDescriptor<float, 2> input2, MemRefDescriptor<float, 2> input3, MemRefDescriptor<float, 2> output);
-  void *main_graph(float* a_allocated, float* a_aligned, int64_t a_offset, int64_t a_size0, int64_t a_size1, int64_t a_stride0, int64_t a_stride1,
-                   float* b_allocated, float* b_aligned, int64_t b_offset, int64_t b_size0, int64_t b_size1, int64_t b_stride0, int64_t b_stride1,
-                   float* c_allocated, float* c_aligned, int64_t c_offset, int64_t c_size0, int64_t c_size1, int64_t c_stride0, int64_t c_stride1,
-                   float* d_allocated, float* d_aligned, int64_t d_offset, int64_t d_size0, int64_t d_size1, int64_t d_stride0, int64_t d_stride1,
-                   float* e_allocated, float* e_aligned, int64_t e_offset, int64_t e_size0, int64_t e_size1, int64_t e_stride0, int64_t e_stride1);
+  void *forward(int64_t* a_allocated, int64_t* a_aligned, int64_t a_offset, int64_t a_size0, int64_t a_size1, int64_t a_stride0, int64_t a_stride1,
+                float* b_allocated, float* b_aligned, int64_t b_offset, int64_t b_size0, int64_t b_size1, int64_t b_stride0, int64_t b_stride1);
 }
 
 
 Simulator* cgra;
 
 int main(int argc, char *argv[]) {
-  float *a = new float[512];
-  float *b = new float[512];
-  float *c = new float[256];
-  float *d = new float[256];
-  float *e = new float[256];
+  int64_t *a = new int64_t[12];
+  float *b = new float[2];
 
-  for (int i=0; i<512; ++i) {
-    a[i] = i;
-  }
-
-  for (int i=0; i<512; ++i) {
-    int x = i / 16;
-    int y = i % 16;
-    if (x == y)
-      b[i] = 1;
-  }
-
-  for (int i=0; i<256; ++i) {
-    d[i] = 1;
-  }
-  
-  // we can use this to describe the memref later
-  MemRefDescriptor<float, 2> memref = {
-    a,    // allocated
-    a,    // aligned
-    0,    // offset
-    {16, 32}, // sizes[N]
-    {1, 1},  // strides[N]
-  };
+  a[0] = 101;
+  a[1] = 1996;
+  a[2] = 4248;
+  a[3] = 2829;
+  a[4] = 4419;
+  a[5] = 14523;
+  a[6] = 2058;
+  a[7] = 1996;
+  a[8] = 13971;
+  a[9] = 3899;
+  a[10] = 1012;
+  a[11] = 102;
 
   cgra = new Simulator(false);
 
-  // main_graph(memref0, memref1, memref2, memref3, memref4);
-  main_graph(a, a, 0, 16, 32, 1, 1, b, b, 0, 32, 16, 1, 1, c, c, 0, 16, 16, 1, 1, d, d, 0, 16, 16, 1, 1, e, e, 0, 16, 16, 1, 1);
+  forward(a, a, 0, 1, 12, 1, 1, b, b, 0, 1, 2, 1, 1);
 
   std::cout<<"check result: "<<std::endl;
-  for (int i=0; i<256; ++i) {
-    std::cout<<e[i]<<"\t";
-    if (i % 16 == 15) {
-      std::cout<<std::endl;
-    }
+  for (int i=0; i<2; ++i) {
+    std::cout<<b[i]<<"\t";
   }
+  std::cout<<std::endl;
+
   return 0;
 }
---
+++ experiments/bert/model/bert.py
@@ -0,0 +1,63 @@
+import torch
+import torch_mlir
+
+from transformers import AutoTokenizer, AutoModelForSequenceClassification
+
+
+def prepare_sentence_tokens(hf_model: str, sentence: str):
+    tokenizer = AutoTokenizer.from_pretrained(hf_model)
+    print(tokenizer)
+    return torch.tensor([tokenizer.encode(sentence)])
+
+
+class OnlyLogitsHuggingFaceModel(torch.nn.Module):
+    """Wrapper that returns only the logits from a HuggingFace model."""
+
+    def __init__(self, model_name: str):
+        super().__init__()
+        self.model = AutoModelForSequenceClassification.from_pretrained(
+            model_name,  # The pretrained model name.
+            # The number of output labels--2 for binary classification.
+            num_labels=2,
+            # Whether the model returns attentions weights.
+            output_attentions=False,
+            # Whether the model returns all hidden-states.
+            output_hidden_states=False,
+            torchscript=True,
+        )
+        self.model.eval()
+
+    def forward(self, input):
+        # Return only the logits.
+        return self.model(input)[0]
+
+
+# Suppress warnings
+import warnings
+warnings.simplefilter("ignore")
+import os
+os.environ["TOKENIZERS_PARALLELISM"] = "true"
+# The HuggingFace model name to use
+model_name = "philschmid/MiniLM-L6-H384-uncased-sst2"
+
+# The sentence to run the model on
+sentence = "The quick brown fox jumps over the lazy dog."
+
+print("Parsing sentence tokens.")
+example_input = prepare_sentence_tokens(model_name, sentence)
+print("Instantiating model.")
+model = OnlyLogitsHuggingFaceModel(model_name)
+print(model)
+linalg_on_tensors_mlir = torch_mlir.compile(
+    model,
+    example_input,
+    output_type=torch_mlir.OutputType.LINALG_ON_TENSORS,
+    use_tracing=True)
+file_path = 'bert.txt'
+new_path = 'bert.mlir'
+with open(file_path, 'wt') as f:
+    print(linalg_on_tensors_mlir.operation.get_asm(large_elements_limit=10), file=f)
+os.rename(file_path,new_path)
+
+result = model.forward(example_input)
+print(result)
